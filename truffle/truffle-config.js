1bd91407254a82bfc10211c9e61e7701f504a826
Jason <jason.iowa@gmail.com>
2021-02-21 01:27:18 -0700

    initial commit

---
+++ truffle/truffle-config.js
@@ -0,0 +1,16 @@
+const Web3 = require('web3');
+const protocol = "http";
+const ip = "localhost";
+const port = 9650;
+module.exports = {
+  networks: {
+   development: {
+     provider: function() {
+      return new Web3.providers.HttpProvider(`${protocol}://${ip}:${port}/ext/bc/C/rpc`)
+     },
+     network_id: "*",
+     gas: 3000000,
+     gasPrice: 470000000000
+   }
+  }
+};
